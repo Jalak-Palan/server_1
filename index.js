f263dc39354b3e5823c46e82a01a4c8ac5487b43
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-02-06 11:35:21 +0530

    update

--- index.js
+++ index.js
@@ -1,59 +1,56 @@
 const express = require("express");
 
 const app = express();
-
-const users = [
+const students = [
   {
-    "studentName": "ABDUL HAQUE",
-    "University": "SUxCG 714",
-    "UniversityUID": "108444"
+    studentName: "ABDUL HAQUE",
+    University: "SUxCG 714",
+    UniversityUID: "108444",
   },
   {
-    "studentName": "ADITYA KUMAR",
-    "University": "SUxCG 702",
-    "UniversityUID": "108716",
+    studentName: "ADITYA KUMAR",
+    University: "SUxCG 702",
+    UniversityUID: "108716",
   },
   {
-    "studentName": "AMAN KUMAR",
-    "University": "SUxCG 702",
-    "UniversityUID": "108500"
+    studentName: "AMAN KUMAR",
+    University: "SUxCG 702",
+    UniversityUID: "108500",
   },
   {
-    "studentName": "AMRIT RAJ",
-    "University": "SUxCG 702",
-    "UniversityUID": "108587"
+    studentName: "AMRIT RAJ",
+    University: "SUxCG 702",
+    UniversityUID: "108587",
   },
   {
-    "studentName": "Jalak Palan",
-    "University": "SUxCG 702",
-    "UniversityUID": "108714"
+    studentName: "Palan Jalak",
+    University: "SUxCG 702",
+    UniversityUID: "108714",
   }
-
-]
+];
 
 app.get("/", (req, res) => {
-  res.send("Express server is running");
+  res.send("CG Students API");
 });
 
-app.get("/users/test/:from_card", (req, res) => {
-  console.log("jalak", req)
-  console.log("jalak", req.params);
-  res.status(200).json(users);
+app.get("/cg/students", (req, res) => {
+  res.status(200).json(students);
 });
 
-app.get("/users/:UniversityUID", (req, res) => {
-  res.status(200).json(users);
-});
+app.get("/cg/students/:gr_number", (req, res) => {
+  const grNumber = req.params.gr_number;
 
-app.get("/users/:id", (req, res) => {
-  const userId = Number(req.params.id);
-  const user = users.find(u => u.id === userId);
+  const student = students.find(
+    (s) => s.UniversityUID === grNumber
+  );
 
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
+  if (!student) {
+    return res.status(404).json({
+      message: "Student not found",
+    });
   }
 
-  res.status(200).json(user);
+  res.status(200).json(student);
 });
 
 app.listen(3000, () => {
